f536acc573e71859735b861061b91d1f160bb0e2
Eugenio Merino <eugeniomerino79@gmail.com>
2020-11-24 18:01:43 -0600

    add resource react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="">
+     <button>Hola</button>
+Holamundo
+    </div>
+  );
+}
+
+export default App;
